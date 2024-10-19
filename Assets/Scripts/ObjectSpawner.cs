fb831ef4ad80aeebe471674b60cb9724dda7830e
Alessiiiiii <alexisgor2000@hotmail.com>
2024-10-19 18:16:56 -0300

    Avance

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
+    public GameObject objectPrefab;
+
+    public Vector3 spawnPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(objectPrefab,spawnPosition,Quaternion.identity);
     }
 
     // Update is called once per frame
