d8fff1e804023f5af80f0ec52a5015894a9564a0
Alessiiiiii <alexisgor2000@hotmail.com>
2024-10-21 20:50:04 -0300

    Avance

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject deadBodyPrefab;
-    // Start is called before the first frame update
-    void Start()
+
+    public int Health;
+    public TextMeshProUGUI enemyHealthUI;
+
+
+    private void Start()
     {
-        
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateUI()
     {
-        
+        enemyHealthUI.text = Health.ToString();
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Bullet"))        {
 
-        };
-        GameObject deadBody = Instantiate(deadBodyPrefab, transform.position, transform.rotation);
-        Destroy(gameObject);
+    
+    public void ChangeHealth(int damage)
+    {
+        Health += damage;
+
+        UpdateUI();
+
+        if (Health <= 0)
         {
-            
+            Destroy(gameObject);
         }
+
     }
 }
