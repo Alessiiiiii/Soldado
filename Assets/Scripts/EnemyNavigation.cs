e76312849a86e5f4394dac2b6824ec79d1cc84c4
Alessiiiiii <alexisgor2000@hotmail.com>
2024-10-27 15:15:42 -0300

    Avance

---
+++ Assets/Scripts/EnemyNavigation.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NavMeshAgent agent;
+    public Transform player;
+    public float intitalDelay;
+    public float interval;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        InvokeRepeating("SetDestination", intitalDelay,interval);
+    }
+
+   
+    public void SetDestination()
+    {
+        {
+
+
+            {
+                Debug.Log("Set destination: " + player.position);
+
+                agent.destination = player.position;
+            }
+        }
+    }
+}
